8b664f01e136fd1c6e4070fe85a0439c31972a6e
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-05-07 00:41:44 +0100

    user db schema

---
+++ Backend/src/auth/models.py
@@ -0,0 +1,14 @@
+# DB Models
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+from ..models import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True)
+    hashed_password = Column(String(255))
+    first_name = Column(String(100))
+    last_name = Column(String(100))

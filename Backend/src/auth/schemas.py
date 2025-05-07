9fdf9ceb9b17dad06ff96eacec667bbec469fcaa
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-05-07 20:52:22 +0100

    added pydantic model for user creation

--- Backend/src/auth/schemas.py
+++ Backend/src/auth/schemas.py
@@ -1,1 +1,12 @@
 # Pydantic Models
+
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    first_name: str
+    last_name: str
+
+    class Config:
+        orm_mode = True

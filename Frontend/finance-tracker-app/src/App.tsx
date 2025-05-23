522fede414016918a8b891de13f834b8e20d8d44
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-05-24 00:22:16 +0100

    added sample grid component and main page layout on app.tsx

--- Frontend/finance-tracker-app/src/App.tsx
+++ Frontend/finance-tracker-app/src/App.tsx
@@ -1,3 +1,4 @@
+import AgGridExample from "./common_components/Grid";
 import Sidebar from "./common_components/Sidebar";
 import SignIn from "./common_components/SignIn";
 import SignUp from "./common_components/signup";
@@ -7,10 +8,17 @@ import { sideBarBotNav, sideBarTopNav } from "./constants/sidebar";
 function App() {
   
   return (
-  <div>
-    <Sidebar topnav={sideBarTopNav} botnav={sideBarBotNav}></Sidebar>
+    
+    <div style={{ display: "flex", height: "100vh", width: "100vw" }}>
+    <div style={{ width: "140px" }}>
+      <Sidebar topnav={sideBarTopNav} botnav={sideBarBotNav} />
+    </div>
+    <div style={{ flex: 1 }}>
+      <AgGridExample />
+    </div>
   </div>
   );
+    
 }
 
 export default App;
---
+++ Frontend/finance-tracker-app/src/common_components/Grid.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import { AllCommunityModule, ModuleRegistry, ColDef } from "ag-grid-community";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+// TODO make it dynamic
+
+const AgGridExample: React.FC = () => {
+    const [columnDefs] = useState<ColDef[]>([
+      { field: "make", sortable: true, filter: true },
+      { field: "model", sortable: true, filter: true },
+      { field: "price", sortable: true, filter: true },
+    ]);
+  
+    const [rowData] = useState([
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxster", price: 72000 },
+    ]);
+  
+    return (
+      <div style={{ height: "100%", width: "100%" }}>
+        <div className="ag-theme-alpine" style={{ height: "100%", width: "100%" }}>
+          <AgGridReact columnDefs={columnDefs} rowData={rowData} />
+        </div>
+      </div>
+    );
+  };
+  
+  export default AgGridExample;

4416ccebc1f84a661183d7ee8737b7b5a1993456
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-05-26 18:14:59 +0100

    linking signup and signin components to each other

--- Frontend/finance-tracker-app/src/App.tsx
+++ Frontend/finance-tracker-app/src/App.tsx
@@ -11,8 +11,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );

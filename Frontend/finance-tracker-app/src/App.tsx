7f7a8e1ba5579aa8af1958dabefd6303cd395c21
Giselle Tan <giselletan1008@gmail.com>
2025-05-30 03:28:48 +0800

    create expenses as pop up

--- Frontend/finance-tracker-app/src/App.tsx
+++ Frontend/finance-tracker-app/src/App.tsx
@@ -3,6 +3,7 @@ import SignUp from "./common_components/SignUp";
 import Dashboard from "./components/dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateExpense from "./common_components/CreateExpense";
+import CreateExpenseTest from "./common_components/CreateExpenseTest";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/expenses" element={<CreateExpenseTest />} />
       </Routes>
     </BrowserRouter>
   );

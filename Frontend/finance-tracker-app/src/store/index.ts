857baf45e5b42a820cb48e297a89ae8fce106323
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-06-29 00:00:14 +0100

    updated store config to add saga & reducer

--- Frontend/finance-tracker-app/src/store/index.ts
+++ Frontend/finance-tracker-app/src/store/index.ts
@@ -1,6 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from '../reducers/rootReducer';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: false}).concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
+
+export type RootState = ReturnType<typeof store.getState>
